f239328772b55fd4029c194d7aef1ce6877a8bb8
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-01-27 11:57:05 -0800

    create operator functions, value display not functional

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 //constants
-
+const DEFAULT_DISPLAY = 0;
 
 //variables
 let num1
 let num2
 let operator
+let displayValue = DEFAULT_DISPLAY;
+
+//Event listeners
+//Startup
+window.addEventListener('DOMContentLoaded', () => {
+    newDisplay();
+  });
 
 //functions
-
 function add(a, b) {
     return a + b;
 };
@@ -27,3 +33,16 @@ function divide(a, b) {
 function operate(num1, num2, operator) {
     return operator(a, b);
 };
+
+function newDisplay() {
+
+    const numbers = document.querySelectorAll('.number');
+
+    numbers.forEach(btnNum => {
+        btnNum.addEventListener('click', () => {
+            displayValue += btnNum.textContent()
+        });
+    });
+
+    return displayValue
+}
