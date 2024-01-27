34d773f97200a18977a86bbba1672496dc2fe3e7
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-01-27 10:38:45 -0800

    create calculator functions and buttons

---
+++ script.js
@@ -0,0 +1,29 @@
+//constants
+
+
+//variables
+let num1
+let num2
+let operator
+
+//functions
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a*b;
+};
+
+function divide(a, b) {
+    return a/b;
+};
+
+function operate(num1, num2, operator) {
+    return operator(a, b);
+};
