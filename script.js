4f924f8cce6c60704a307dcadfc4b7709c241139
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-01-27 23:20:00 -0800

    need to find a way to clear number, need to create store total variable

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 //constants
-const DEFAULT_DISPLAY = 0;
+const DEFAULT_VALUE = 0;
 
 //variables
 let num1
 let num2
-let operator
-let displayValue = DEFAULT_DISPLAY;
+let operator = '';
+let displayValue = DEFAULT_VALUE;
 
 //Event listeners
 //Startup
 window.addEventListener('DOMContentLoaded', () => {
-    newDisplay();
+    clearDisplay()
+    numberInput()
+    userOperator()
   });
-
-
 
 //functions
 function add(a, b) {
@@ -32,24 +32,71 @@ function divide(a, b) {
     return a/b;
 };
 
-function operate(num1, num2, operator) {
-    return operator(a, b);
+function operate(a, b, operator) {  
+    a = Number(a);
+    b = Number(b);
+    
+    if (operator == '+') {
+        return add(a,b);
+    } else if (operator == '-') {
+        return subtract(a,b);
+    } else if (operator == '*') {
+        return multiply(a,b);
+    } else if (operator == '/') {
+        return divide(a,b);
+    };
 };
 
-function newDisplay() {
+function numberInput() {
     const numbers = document.querySelectorAll('.number');
-    displayValue = '';
     numbers.forEach(btnNum => {
         btnNum.addEventListener('click', () => {            
+            //removes initial 0 value
+            if (displayValue == 0) {
+                displayValue = '';
+            }
             //verifies only one decimal is allowed per entry
             if (btnNum.textContent !== '.' ||
             !displayValue.includes('.')) {
             displayValue += btnNum.textContent;
-
-            document.querySelector('.display').textContent
-            = displayValue;
+            updateDisplay(displayValue);
             };
         });
     });
     return Number(displayValue);
+};
+
+function updateDisplay(text = DEFAULT_VALUE) {
+    document.querySelector('.display').textContent = text;
+};
+
+function clearDisplay() {
+    document.querySelector('.clear').onclick = () => {
+        displayValue = DEFAULT_VALUE;
+        updateDisplay();
+    }; 
+};
+
+function userOperator() {
+    const btnOperator = document.querySelectorAll('.operator');
+
+    btnOperator.forEach(input => {
+        input.addEventListener('click', () => {
+            if (operator !== '' && operator !== '=') {
+                num2 = displayValue;
+                displayValue = operate(num1, num2, operator)
+                updateDisplay(displayValue)
+            } else if (operator !== '=' && input.textContent == '=') {
+                num2 = displayValue;
+                displayValue = operate(num1, num2, operator)
+                updateDisplay(displayValue)
+                operator = '';
+            }
+            num1 = displayValue;
+            operator = input.textContent;
+            displayValue = '';
+
+        })
+    })
+    
 }
